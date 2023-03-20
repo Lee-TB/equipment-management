dcf93152602908bf802df08c193cd528d2e656cc
Duc Tran <tblyduc2412@gmail.com>
2023-03-20 14:02:51 +0700

    complete setup tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
-  declarations: [AppComponent, AuthLayoutComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
