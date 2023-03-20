4b9ffad6b55d1ee5818a0f22de70a488f835f580
Duc Tran <tblyduc2412@gmail.com>
2023-03-20 17:10:14 +0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layouts/content-layout/content-layout.component';
+import { PageNotFoundComponent } from './layouts/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -9,10 +11,30 @@ const routes: Routes = [
     pathMatch: 'full',
   },
   {
+    path: '',
+    component: ContentLayoutComponent,
+    canActivate: [], // Should be replaced with actual auth guard
+    children: [
+      {
+        path: 'equipments',
+        loadChildren: () =>
+          import('./modules/equipment/equipment.module').then(
+            (m) => m.EquipmentModule
+          ),
+      },
+    ],
+  },
+  {
     path: 'auth',
     component: AuthLayoutComponent,
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  // Fallback when no prior routes is matched
+  { path: '**', redirectTo: '/page-not-found', pathMatch: 'full' },
+  {
+    path: 'page-not-found',
+    component: PageNotFoundComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layouts/content-layout/content-layout.component';
+import { PageNotFoundComponent } from './layouts/page-not-found/page-not-found.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
-  declarations: [AppComponent, AuthLayoutComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  declarations: [
+    AppComponent,
+    AuthLayoutComponent,
+    ContentLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
