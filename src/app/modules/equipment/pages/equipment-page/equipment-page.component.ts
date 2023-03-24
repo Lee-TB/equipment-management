41d61877d0ee673b49fe263f273bf6c8c5a43782
Duc Tran <tblyduc2412@gmail.com>
2023-03-24 16:13:13 +0700

    style: equipment tabs selected

--- src/app/modules/equipment/pages/equipment-page/equipment-page.component.ts
+++ src/app/modules/equipment/pages/equipment-page/equipment-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EquipmentService } from 'src/app/shared/services/equipment/equipment.service';
+import { Location } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-equipment-page',
   templateUrl: './equipment-page.component.html',
-  styleUrls: ['./equipment-page.component.css']
+  styleUrls: ['./equipment-page.component.css'],
 })
-export class EquipmentPageComponent {
+export class EquipmentPageComponent implements OnInit {
+  selectedTab: 'table' | 'new' = 'table';
+  currentPath = this.location.path();
 
+  constructor(
+    private equipmentService: EquipmentService,
+    private location: Location,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.equipmentService.getAll();
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentPath = event.urlAfterRedirects;
+        this.selectedTab = <'table' | 'new'>this.currentPath.split('/')[2];
+      }
+    });
+  }
+
+  ngOnDestroy() {}
 }
