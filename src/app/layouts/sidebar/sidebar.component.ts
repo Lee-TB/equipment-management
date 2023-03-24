21ba35f22c9179457fe1f8b5a5f626097b413093
Duc Tran <tblyduc2412@gmail.com>
2023-03-24 16:30:40 +0700

    style: sidebar tabs selected

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { UserService } from 'src/app/shared/services/user/user.service';
+
+type TabType = 'dashboard' | 'equipments' | 'users';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   isLoggedIn: boolean = this.userService.isLoggedIn();
-  constructor(private userService: UserService) {}
+  selectedTab?: TabType;
+  currentPath = '';
+  constructor(
+    private userService: UserService,
+    private location: Location,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentPath = event.urlAfterRedirects;
+        this.selectedTab = <TabType>this.currentPath.split('/')[1];
+      }
+    });
+  }
 }
