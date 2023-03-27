0e9416b4a78069283faf1543d470aeb36f090f89
Duc Tran <tblyduc2412@gmail.com>
2023-03-27 14:22:17 +0700

    feat: add equipment form use reactive form

--- src/app/modules/equipment/equipment.module.ts
+++ src/app/modules/equipment/equipment.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { EquipmentRoutingModule } from './equipment-routing.module';
 import { EquipmentTableComponent } from './components/equipment-table/equipment-table.component';
-import { TableComponent } from 'src/app/shared/components/table/table.component';
 import { EquipmentPageComponent } from './pages/equipment-page/equipment-page.component';
+import { EquipmentFormComponent } from './components/equipment-form/equipment-form.component';
 
 @NgModule({
-  imports: [CommonModule, EquipmentRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, EquipmentRoutingModule],
   declarations: [
     EquipmentPageComponent,
     EquipmentTableComponent,
-    TableComponent,
+    EquipmentFormComponent,
   ],
 })
 export class EquipmentModule {}
