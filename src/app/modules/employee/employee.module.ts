a8a2cef6af0b546db4ca763496616390fec7f5d3
Duc Tran <tblyduc2412@gmail.com>
2023-03-29 11:15:55 +0700

    import ReactiveFormsModule and declare EmployeeFormComponent.

--- src/app/modules/employee/employee.module.ts
+++ src/app/modules/employee/employee.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeePageComponent } from './pages/employee-page/employee-page.component';
 import { EmployeeTableComponent } from './components/employee-table/employee-table.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
 
 @NgModule({
-  imports: [CommonModule, EmployeeRoutingModule],
-  declarations: [EmployeePageComponent, EmployeeTableComponent],
+  imports: [CommonModule, ReactiveFormsModule, EmployeeRoutingModule],
+  declarations: [
+    EmployeePageComponent,
+    EmployeeTableComponent,
+    EmployeeFormComponent,
+  ],
 })
 export class EmployeeModule {}
