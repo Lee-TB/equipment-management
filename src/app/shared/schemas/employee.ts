c6c7ccbda3f53f22374ac893ac16770e1157592b
Duc Tran <tblyduc2412@gmail.com>
2023-03-29 14:09:27 +0700

    add: employee interface

---
+++ src/app/shared/schemas/employee.ts
@@ -0,0 +1,10 @@
+export interface employee {
+  FullName: string;
+  RoleName: string;
+  Username: string;
+  Password: string;
+  PhoneNumber: string;
+  Email: string;
+  Image: string;
+  DepartmentName: string;
+}
