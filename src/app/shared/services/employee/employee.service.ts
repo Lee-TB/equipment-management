a94147d54ed6c937eb4951788693ba04c37ad75a
Duc Tran <tblyduc2412@gmail.com>
2023-04-03 15:15:15 +0700

    refactor: employee service

--- src/app/shared/services/employee/employee.service.ts
+++ src/app/shared/services/employee/employee.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, Inject } from '@angular/core';
-import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -16,48 +15,37 @@ export class EmployeeService {
   ) {}
 
   getEmloyeesByPaging(pageNumber: number = 1, pageSize: number = 5) {
-    return this.http
-      .get(
-        this.baseURL +
-          `api/users/paging?PageNumber=${pageNumber}&PageSize=${pageSize}`,
-        this.httpOptions
-      )
-      .pipe(tap((res: any) => {}));
+    return this.http.get(
+      this.baseURL +
+        `api/users/paging?PageNumber=${pageNumber}&PageSize=${pageSize}`,
+      this.httpOptions
+    );
   }
 
   getRoles() {
-    return this.http
-      .get(this.baseURL + `api/roles`, this.httpOptions)
-      .pipe(tap((res: any) => {}));
+    return this.http.get(this.baseURL + `api/roles`, this.httpOptions);
   }
 
   getDepartments() {
-    return this.http
-      .get(this.baseURL + `api/departments`, this.httpOptions)
-      .pipe(tap((res: any) => {}));
+    return this.http.get(this.baseURL + `api/departments`, this.httpOptions);
   }
 
   addAnEmployee(formData: FormData) {
-    return this.http
-      .post(this.baseURL + `api/auth/register`, formData)
-      .pipe(tap((res: any) => {}));
+    return this.http.post(this.baseURL + `api/auth/register`, formData);
   }
 
   updateAnEmployee(employeeId: number, formData: FormData) {
-    return this.http
-      .put(this.baseURL + `api/users/${employeeId}`, formData)
-      .pipe(tap((res: any) => {}));
+    return this.http.put(this.baseURL + `api/users/${employeeId}`, formData);
   }
 
   getAnEmployee(employeeId: number) {
-    return this.http
-      .get(this.baseURL + `api/users/${employeeId}`)
-      .pipe(tap((res: any) => {}));
+    return this.http.get(this.baseURL + `api/users/${employeeId}`);
   }
 
   removeAnEmployee(employeeId: number) {
-    return this.http
-      .delete(this.baseURL + `api/users/${employeeId}`, this.httpOptions)
-      .pipe(tap((res: any) => {}));
+    return this.http.delete(
+      this.baseURL + `api/users/${employeeId}`,
+      this.httpOptions
+    );
   }
 }
